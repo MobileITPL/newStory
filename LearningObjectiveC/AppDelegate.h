3f93001b9628a474fa766d14c26628cdb56c16f3
Atirek Pothiwala <mobile@indapoint.com>
2022-07-18 13:01:42 +0530

    Initial Commit

---
+++ LearningObjectiveC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LearningObjectiveC
+//
+//  Created by Indapoint on 18/07/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LearningObjectiveC/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LearningObjectiveC
+//
+//  Created by Indapoint on 18/07/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
